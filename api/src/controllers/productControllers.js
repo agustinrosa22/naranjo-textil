3b678dedd64ede1e523e71e3fa99b3c3dd9397a0
agustinrosa22 <agustinrosa1234@gmail.com>
2024-01-16 17:24:50 -0300

    postProduct

--- api/src/controllers/productControllers.js
+++ api/src/controllers/productControllers.js
@@ -1,2 +1,31 @@
 const axios = require('axios')
 const {Product} = require('../db')
+require('dotenv').config()
+//crear producto faltan completar varios aspectos para la integridad de los productos
+const createProduct = async ({
+    nombreProducto,
+    productoId,
+    medidas,
+    proveedor,
+    proveedorId,
+    cantidad,
+    fecha,
+    costo,
+    costoPrevio,
+  }) => {
+  const created = await Product.create({ nombreProducto,
+    productoId,
+    medidas,
+    proveedor,
+    proveedorId,
+    cantidad,
+    fecha,
+    costo,
+    costoPrevio,})
+
+    return created
+  };
+  
+  module.exports = {
+    createProduct,
+  };
